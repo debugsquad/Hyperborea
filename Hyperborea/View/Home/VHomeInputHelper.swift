21458e2850cd456df6bf7e7c9f7496d8e5133f5d
vauxhall <zero.griffin@gmail.com>
2016-12-04 12:50:37 -0600

    helper init

--- Hyperborea/View/Home/VHomeInputHelper.swift
+++ Hyperborea/View/Home/VHomeInputHelper.swift
@@ -2,5 +2,14 @@ import UIKit
 
 class VHomeInputHelper:UIView
 {
+    private weak var controller:CHome!
     
+    convenience init(controller:CHome)
+    {
+        self.init()
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        translatesAutoresizingMaskIntoConstraints = false
+        self.controller = controller
+    }
 }
