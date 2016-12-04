17020d2d6edd7cf0162659dae7c54111535303be
vauxhall <zero.griffin@gmail.com>
2016-12-04 13:15:06 -0600

    blur effect in helper

--- Hyperborea/View/Home/VHomeInputHelper.swift
+++ Hyperborea/View/Home/VHomeInputHelper.swift
@@ -11,5 +11,13 @@ class VHomeInputHelper:UIView
         backgroundColor = UIColor.clear
         translatesAutoresizingMaskIntoConstraints = false
         self.controller = controller
+        
+        let blurEffect:UIBlurEffect = UIBlurEffect(style:UIBlurEffectStyle.light)
+        let visualEffect:UIVisualEffectView = UIVisualEffectView(effect:blurEffect)
+        visualEffect.translatesAutoresizingMaskIntoConstraints = false
+        visualEffect.clipsToBounds = true
+        visualEffect.isUserInteractionEnabled = false
+        
+        addSubview(visualEffect)
     }
 }
