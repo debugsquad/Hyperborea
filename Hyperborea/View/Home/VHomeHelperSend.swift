ec10391e6e6ec094da77934f6230e9c374044380
vauxhall <zero.griffin@gmail.com>
2016-12-04 13:34:08 -0600

    home helper send

---
+++ Hyperborea/View/Home/VHomeHelperSend.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class VHomeHelperSender:UIButton
+{
+    private weak var controller:CHome!
+    
+    convenience init(controller:CHome)
+    {
+        self.init()
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        translatesAutoresizingMaskIntoConstraints = false
+        self.controller = controller
+    }
+}
