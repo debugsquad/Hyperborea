74b927c31fc9352a791d5832b4cb9fa16b74c9c3
vauxhall <zero.griffin@gmail.com>
2016-12-05 10:45:30 -0600

    settings factory

--- Hyperborea/Request/Main/RSettings.swift
+++ Hyperborea/Request/Main/RSettings.swift
@@ -7,28 +7,23 @@ class RSettings
         case unknown
     }
     
+    static let kTimeOut:TimeInterval = 20
     let settingsId:SettingsId
     let urlString:String
+    let timeOut:TimeInterval
     
-    var settingsId:RequestSettingsId = RequestSettingsId.unknown
-    var urlString:String = ""
-    var parser:RequestParser = RequestParser()
-    var type:RequestSettingsType = RequestSettingsType.Get()
-    var timeout:TimeInterval
-    var multipart:Bool = false
-    var token:Bool = false
-    var body:Any?
-    let urlList:[String:String]
-    let kBoundary:String = "SuggesticBoundaryFormPostingData"
-    private let kTimeout:TimeInterval = 24
+    init(
+        settingsId:SettingsId,
+        urlString:String,
+        timeOut:TimeInterval = kTimeOut)
+    {
+        self.settingsId = settingsId
+        self.urlString = urlString
+        self.timeOut = timeOut
+    }
     
     init()
     {
-        let resURL:URL = Bundle.main.url(
-            forResource:"resourcesUrls",
-            withExtension:"plist")!
-        let urlsDictionary:NSDictionary = NSDictionary(contentsOf:resURL)!
-        urlList = urlsDictionary as! [String:String]
-        timeout = kTimeout
+        fatalError()
     }
 }
