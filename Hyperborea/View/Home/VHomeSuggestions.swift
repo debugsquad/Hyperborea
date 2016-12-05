7a29acb5c483d5432440c24a0cd5590fd8b5659d
vauxhall <zero.griffin@gmail.com>
2016-12-05 15:41:43 -0600

    view home suggestions

---
+++ Hyperborea/View/Home/VHomeSuggestions.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class VHomeSuggestions:UIView
+{
+    private weak var controller:CHome!
+    
+    convenience init(controller:CHome)
+    {
+        self.init()
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        translatesAutoresizingMaskIntoConstraints = false
+        self.controller = controller
+    }
+}
