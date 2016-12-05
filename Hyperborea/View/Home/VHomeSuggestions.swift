67bfd08fc8053c8912bbff42796b4b0148700a14
vauxhall <zero.griffin@gmail.com>
2016-12-05 16:17:05 -0600

    collection view and constraints

--- Hyperborea/View/Home/VHomeSuggestions.swift
+++ Hyperborea/View/Home/VHomeSuggestions.swift
@@ -1,8 +1,9 @@
 import UIKit
 
-class VHomeSuggestions:UIView
+class VHomeSuggestions:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
     private weak var controller:CHome!
+    private weak var collectionView:UICollectionView!
     
     convenience init(controller:CHome)
     {
@@ -18,7 +19,33 @@ class VHomeSuggestions:UIView
         visualEffect.clipsToBounds = true
         visualEffect.isUserInteractionEnabled = false
         
+        let flow:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        flow.headerReferenceSize = CGSize.zero
+        flow.footerReferenceSize = CGSize.zero
+        flow.scrollDirection = UICollectionViewScrollDirection.horizontal
+        flow.minimumLineSpacing = 0
+        flow.minimumInteritemSpacing = 0
+        flow.sectionInset = UIEdgeInsets.zero
+        
+        let collectionView:UICollectionView = UICollectionView(
+            frame:CGRect.zero,
+            collectionViewLayout:flow)
+        collectionView.clipsToBounds = true
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = UIColor.clear
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.alwaysBounceHorizontal = true
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(
+            VHomeSuggestionsCell.self,
+            forCellWithReuseIdentifier:
+            VHomeSuggestionsCell.reusableIdentifier)
+        self.collectionView = collectionView
+        
         addSubview(visualEffect)
+        addSubview(collectionView)
         
         let layoutVisualEffectTop:NSLayoutConstraint = NSLayoutConstraint(
             item:visualEffect,
@@ -53,10 +80,47 @@ class VHomeSuggestions:UIView
             multiplier:1,
             constant:0)
         
+        let layoutCollectionViewTop:NSLayoutConstraint = NSLayoutConstraint(
+            item:collectionView,
+            attribute:NSLayoutAttribute.top,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.top,
+            multiplier:1,
+            constant:0)
+        let layoutCollectionViewBottom:NSLayoutConstraint = NSLayoutConstraint(
+            item:collectionView,
+            attribute:NSLayoutAttribute.bottom,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.bottom,
+            multiplier:1,
+            constant:0)
+        let layoutCollectionViewLeft:NSLayoutConstraint = NSLayoutConstraint(
+            item:collectionView,
+            attribute:NSLayoutAttribute.left,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.left,
+            multiplier:1,
+            constant:0)
+        let layoutCollectionViewRight:NSLayoutConstraint = NSLayoutConstraint(
+            item:collectionView,
+            attribute:NSLayoutAttribute.right,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.right,
+            multiplier:1,
+            constant:0)
+        
         addConstraints([
             layoutVisualEffectTop,
             layoutVisualEffectBottom,
             layoutVisualEffectLeft,
-            layoutVisualEffectRight])
+            layoutVisualEffectRight,
+            layoutCollectionViewTop,
+            layoutCollectionViewBottom,
+            layoutCollectionViewLeft,
+            layoutCollectionViewRight])
     }
 }
