0ca8792923281fb8d071d2b5db36af44a1b6fc8a
vauxhall <zero.griffin@gmail.com>
2016-12-05 15:50:38 -0600

    view home suggestions cell

---
+++ Hyperborea/View/Home/VHomeSuggestionsCell.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class VHomeSuggestionsCell:UICollectionViewCell
+{
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
+}
