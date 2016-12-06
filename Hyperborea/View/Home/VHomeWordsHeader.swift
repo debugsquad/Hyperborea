82113eb14408ca0c623998c9bdfd8dd37ac75b9b
vauxhall <zero.griffin@gmail.com>
2016-12-06 15:07:22 -0600

    button and constraints

--- Hyperborea/View/Home/VHomeWordsHeader.swift
+++ Hyperborea/View/Home/VHomeWordsHeader.swift
@@ -8,7 +8,53 @@ class VHomeWordsHeader:UICollectionReusableView
     {
         super.init(frame:frame)
         clipsToBounds = true
-        backgroundColor = UIColor.genericBlue
+        backgroundColor = UIColor.clear
+        
+        let button:UIButton = UIButton()
+        button.clipsToBounds = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = UIColor.clear
+        
+        addSubview(button)
+        
+        let layoutButtonTop:NSLayoutConstraint = NSLayoutConstraint(
+            item:button,
+            attribute:NSLayoutAttribute.top,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.top,
+            multiplier:1,
+            constant:0)
+        let layoutButtonBottom:NSLayoutConstraint = NSLayoutConstraint(
+            item:button,
+            attribute:NSLayoutAttribute.bottom,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.bottom,
+            multiplier:1,
+            constant:0)
+        let layoutButtonLeft:NSLayoutConstraint = NSLayoutConstraint(
+            item:button,
+            attribute:NSLayoutAttribute.left,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.left,
+            multiplier:1,
+            constant:0)
+        let layoutButtonRight:NSLayoutConstraint = NSLayoutConstraint(
+            item:button,
+            attribute:NSLayoutAttribute.right,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.right,
+            multiplier:1,
+            constant:0)
+        
+        addConstraints([
+            layoutButtonTop,
+            layoutButtonBottom,
+            layoutButtonLeft,
+            layoutButtonRight])
     }
     
     required init?(coder:NSCoder)
