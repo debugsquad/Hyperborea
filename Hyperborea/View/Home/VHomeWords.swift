6fdfe843201d103516c13accecbe1259462facd9
vauxhall <zero.griffin@gmail.com>
2016-12-06 14:06:36 -0600

    model home entries and items

--- Hyperborea/View/Home/VHomeWords.swift
+++ Hyperborea/View/Home/VHomeWords.swift
@@ -1,8 +1,9 @@
 import UIKit
 
-class VHomeWords:UIView
+class VHomeWords:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
     private weak var controller:CHome!
+    private weak var collectionView:UICollectionView!
     
     convenience init(controller:CHome)
     {
@@ -11,5 +12,26 @@ class VHomeWords:UIView
         backgroundColor = UIColor.clear
         translatesAutoresizingMaskIntoConstraints = false
         self.controller = controller
+    }
+    
+    //MARK: collectionView delegate
+    
+    func collectionView(_ collectionView:UICollectionView, layout collectionViewLayout:UICollectionViewLayout, insetForSectionAt section:Int) -> UIEdgeInsets
+    {
+        let 
+    }
+    
+    func numberOfSections(in collectionView:UICollectionView) -> Int
+    {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
     }
 }
