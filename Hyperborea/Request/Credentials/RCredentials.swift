785330dfdb4b70d9b7d26395b53458d81ad90197
vauxhall <zero.griffin@gmail.com>
2016-12-05 20:29:25 -0600

    credentials

---
+++ Hyperborea/Request/Credentials/RCredentials.swift
@@ -0,0 +1,9 @@
+//
+//  RCredentials.swift
+//  Hyperborea
+//
+//  Created by zero on 12/5/16.
+//  Copyright © 2016 iturbide. All rights reserved.
+//
+
+import Foundation
---
+++ Hyperborea/Request/Credentials/RCredentialsItem1.swift
@@ -0,0 +1,9 @@
+//
+//  RCredentialsItem1.swift
+//  Hyperborea
+//
+//  Created by zero on 12/5/16.
+//  Copyright © 2016 iturbide. All rights reserved.
+//
+
+import Foundation
