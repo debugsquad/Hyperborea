dce558081fa482652276b5591467a6c2be1ae23b
vauxhall <zero.griffin@gmail.com>
2016-12-07 16:54:44 -0600

    etymologies attributed string

--- Hyperborea/Request/Model/Entries/Abstract/RModelHomeEntriesEtymologies.swift
+++ Hyperborea/Request/Model/Entries/Abstract/RModelHomeEntriesEtymologies.swift
@@ -1,11 +1,11 @@
-import Foundation
+import UIKit
 
 class RModelHomeEntriesEtymologies
 {
-    private let subsenses:[RModelHomeEntriesSensesSub]?
     private let titles:[String]?
     private let kKeyEntries:String = "entries"
-    private let kKeyEtimologies:String = "etimologies"
+    private let kKeyEtymologies:String = "etymologies"
+    private let kEtymologieSeparator:String = "\n"
     
     init(json:Any)
     {
@@ -13,8 +13,7 @@ class RModelHomeEntriesEtymologies
             
             let jsonMap:[String:Any] = json as? [String:Any],
             let jsonEntries:[Any] = jsonMap[kKeyEntries] as? [Any],
-            let jsonEntriesFirst:[String:Any] = jsonEntries.first as? [String:Any],
-            let jsonEtimologies:[String] = jsonEntriesFirst[kKeyEtimologies] as? [String]
+            let jsonEntriesFirst:[String:Any] = jsonEntries.first as? [String:Any]
             
         else
         {
@@ -23,57 +22,7 @@ class RModelHomeEntriesEtymologies
             return
         }
         
-        if let sensesDefinitions:[String] = jsonSensesFirst[kKeyDefinitions] as? [String]
-        {
-            self.title = sensesDefinitions.first
-        }
-        else
-        {
-            self.title = nil
-        }
-        
-        if let sensesExamples:[Any] = jsonSensesFirst[kKeyExamples] as? [Any]
-        {
-            var examples:[String] = []
-            
-            for sensesExample:Any in sensesExamples
-            {
-                guard
-                    
-                    let sensesExampleMap:[String:Any] = sensesExample as? [String:Any],
-                    let exampleText:String = sensesExampleMap[kKeyExampleText] as? String
-                    
-                    else
-                {
-                    continue
-                }
-                
-                examples.append(exampleText)
-            }
-            
-            self.examples = examples
-        }
-        else
-        {
-            self.examples = nil
-        }
-        
-        if let jsonSubsenses:[Any] = jsonSensesFirst[kKeySubsenses] as? [Any]
-        {
-            var subsenses:[RModelHomeEntriesSensesSub] = []
-            
-            for jsonSubsense:Any in jsonSubsenses
-            {
-                let subsense:RModelHomeEntriesSensesSub = RModelHomeEntriesSensesSub(json:jsonSubsense)
-                subsenses.append(subsense)
-            }
-            
-            self.subsenses = subsenses
-        }
-        else
-        {
-            self.subsenses = nil
-        }
+        self.titles = jsonEntriesFirst[kKeyEtymologies] as? [String]
     }
     
     //MARK: public
@@ -82,52 +31,44 @@ class RModelHomeEntriesEtymologies
     {
         let mutableString:NSMutableAttributedString = NSMutableAttributedString()
         
-        if let title:String = self.title
+        if let titles:[String] = self.titles
         {
-            let attributes:[String:Any] = [
-                NSFontAttributeName:UIFont.regular(
-                    size:RModelHomeEntriesItem.kDefinitionFontSize),
-                NSForegroundColorAttributeName:UIColor(
-                    white:RModelHomeEntriesItem.kDefinitionWhite,
-                    alpha:1)
-            ]
-            
-            let titleSeparator:String = "\(kSensesSeparator)\(title)"
-            let titleString:NSAttributedString = NSAttributedString(
-                string:titleSeparator,
-                attributes:attributes)
-            
-            mutableString.append(titleString)
-        }
-        
-        if let examples:[String] = self.examples
-        {
-            let attributes:[String:Any] = [
-                NSFontAttributeName:UIFont.italic(
-                    size:RModelHomeEntriesItem.kExampleFontSize),
-                NSForegroundColorAttributeName:UIColor(
-                    white:RModelHomeEntriesItem.kExampleWhite,
-                    alpha:1)
-            ]
-            
-            for example:String in examples
+            if !titles.isEmpty
             {
-                let exampleSeparator:String = "\(kExampleSeparator)\(example)"
-                let exampleString:NSAttributedString = NSAttributedString(
-                    string:exampleSeparator,
-                    attributes:attributes)
+                let attributesSection:[String:Any] = [
+                    NSFontAttributeName:UIFont.medium(
+                        size:RModelHomeEntriesItem.kDefinitionFontSize),
+                    NSForegroundColorAttributeName:UIColor(
+                        white:RModelHomeEntriesItem.kDefinitionWhite,
+                        alpha:1)
+                ]
                 
-                mutableString.append(exampleString)
-            }
-        }
-        
-        if let subsenses:[RModelHomeEntriesSensesSub] = subsenses
-        {
-            for subsense:RModelHomeEntriesSensesSub in subsenses
-            {
-                let senseString:NSAttributedString = subsense.attributedString()
+                let sectionTitle:String = NSLocalizedString(
+                    "RModelHomeEntriesEtymologies_sectionTitle",
+                    comment:"")
+                let sectionTitleString:NSAttributedString = NSAttributedString(
+                    string:sectionTitle,
+                    attributes:attributesSection)
                 
-                mutableString.append(senseString)
+                mutableString.append(sectionTitleString)
+                
+                for title:String in titles
+                {
+                    let attributes:[String:Any] = [
+                        NSFontAttributeName:UIFont.regular(
+                            size:RModelHomeEntriesItem.kDefinitionFontSize),
+                        NSForegroundColorAttributeName:UIColor(
+                            white:RModelHomeEntriesItem.kDefinitionWhite,
+                            alpha:1)
+                    ]
+                    
+                    let titleSeparator:String = "\(kEtymologieSeparator)\(title)"
+                    let titleString:NSAttributedString = NSAttributedString(
+                        string:titleSeparator,
+                        attributes:attributes)
+                    
+                    mutableString.append(titleString)
+                }
             }
         }
         
