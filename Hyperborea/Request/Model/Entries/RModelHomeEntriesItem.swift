629e0de5acfa9fe3f93d113d61796b08fd0e84c3
vauxhall <zero.griffin@gmail.com>
2016-12-07 09:48:57 -0600

    entries item init with properties

--- Hyperborea/Request/Model/Entries/RModelHomeEntriesItem.swift
+++ Hyperborea/Request/Model/Entries/RModelHomeEntriesItem.swift
@@ -2,12 +2,22 @@ import Foundation
 
 class RModelHomeEntriesItem
 {
+    let attributedString:NSAttributedString
+    let reusableIdentifier:String
+    let selectable:Bool
+    
     required init(json:Any)
     {
         fatalError()
     }
     
-    init()
+    init(
+        attributedString:NSAttributedString,
+        reusableIdentifier:String,
+        selectable:Bool)
     {
+        self.attributedString = attributedString
+        self.reusableIdentifier = reusableIdentifier
+        self.selectable = selectable
     }
 }
