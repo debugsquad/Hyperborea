4903a1ee1c84f3bbd78bc7671defc2842e02b656
vauxhall <zero.griffin@gmail.com>
2016-12-07 14:37:56 -0600

    parsing subsenses

--- Hyperborea/Request/Model/Entries/Abstract/RModelHomeEntriesSensesSub.swift
+++ Hyperborea/Request/Model/Entries/Abstract/RModelHomeEntriesSensesSub.swift
@@ -2,12 +2,60 @@ import UIKit
 
 class RModelHomeEntriesSensesSub
 {
-    private let title:String
-    private let examples:[String]
+    private let title:String?
+    private let examples:[String]?
+    private let kKeyDefinitions:String = "definitions"
+    private let kKeyExamples:String = "examples"
+    private let kKeyExampleText:String = "text"
     
     init(json:Any)
     {
+        guard
         
+            let jsonMap:[String:Any] = json as? [String:Any]
+        
+        else
+        {
+            title = nil
+            examples = nil
+            
+            return
+        }
+        
+        if let jsonDefinitions:[String] = jsonMap[kKeyDefinitions] as? [String]
+        {
+            self.title = jsonDefinitions.first
+        }
+        else
+        {
+            self.title = nil
+        }
+        
+        if let jsonExamples:[Any] = jsonMap[kKeyExamples] as? [Any]
+        {
+            var examples:[String] = []
+            
+            for jsonExample:Any in jsonExamples
+            {
+                guard
+                    
+                    let exampleMap:[String:Any] = jsonExample as? [String:Any],
+                    let exampleText:String = exampleMap[kKeyExampleText] as? String
+                    
+                    else
+                {
+                    continue
+                }
+                
+                examples.append(exampleText)
+            }
+            
+            self.examples = examples
+        }
+        else
+        {
+            self.examples = nil
+        }
     }
     
     //MARK: public
