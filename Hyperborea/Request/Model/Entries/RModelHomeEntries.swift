252efdf74a1eb29b252a29605d17fb6e4203cad4
vauxhall <zero.griffin@gmail.com>
2016-12-06 18:52:50 -0600

    parsing json entries

--- Hyperborea/Request/Model/Entries/RModelHomeEntries.swift
+++ Hyperborea/Request/Model/Entries/RModelHomeEntries.swift
@@ -4,19 +4,41 @@ class RModelHomeEntries:RModel
 {
     let items:[RModelHomeEntriesItem]
     private let kKeyResults:String = "results"
-    private let kFontSizeCategory:CGFloat = 14
-    private let kMaxWidth:CGFloat = 2000
-    private let kMaxHeight:CGFloat = 20
+    private let kKeyLexicalEntries:String = "lexicalEntries"
+    private let kKeyLexicalCategory:String = "lexicalCategory"
     
     required init(json:Any)
     {
         var items:[RModelHomeEntriesItem] = []
         
-        let jsonMap:[String:Any]? = json as? [String:Any]
-        
-        if let jsonResults:[Any] = jsonMap?[kKeyResults] as? [Any]
-        {
+        guard
             
+            let jsonMap:[String:Any] = json as? [String:Any],
+            let jsonResults:[Any] = jsonMap[kKeyResults] as? [Any],
+            let jsonFirstResult:[String:Any] = jsonResults.first as? [String:Any],
+            let jsonLexicalEntries:[Any] = jsonFirstResult[kKeyLexicalEntries] as? [Any]
+        
+        else
+        {
+            self.items = items
+            super.init()
+            
+            return
+        }
+        
+        for jsonEntry:Any in jsonLexicalEntries
+        {
+            guard
+            
+                let jsonEntryMap:[String:Any] = jsonEntry as? [String:Any],
+                let jsonEntryCategory:String = jsonEntryMap[kKeyLexicalCategory] as? String
+            
+            else
+            {
+                continue
+            }
+            
+            print("category: \(jsonEntryCategory)")
         }
         
         self.items = items
