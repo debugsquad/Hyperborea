f61c9c41bc2ee5a8789271b20b900b29e0fbc05f
vauxhall <zero.griffin@gmail.com>
2016-12-07 15:03:55 -0600

    attributed string for subsenses

--- Hyperborea/Request/Model/Entries/Abstract/RModelHomeEntriesSensesSub.swift
+++ Hyperborea/Request/Model/Entries/Abstract/RModelHomeEntriesSensesSub.swift
@@ -7,6 +7,12 @@ class RModelHomeEntriesSensesSub
     private let kKeyDefinitions:String = "definitions"
     private let kKeyExamples:String = "examples"
     private let kKeyExampleText:String = "text"
+    private let kSubsenseSeparator:String = "\n"
+    private let kExampleSeparator:String = "\n○ "
+    private let kTitleFontSize:CGFloat = 16
+    private let kExampleFontSize:CGFloat = 14
+    private let kTitleWhite:CGFloat = 0.6
+    private let kExampleWhite:CGFloat = 0.7
     
     init(json:Any)
     {
@@ -62,6 +68,43 @@ class RModelHomeEntriesSensesSub
     
     func attributedString() -> NSAttributedString
     {
+        let mutableString:NSMutableAttributedString = NSMutableAttributedString()
         
+        if let title:String = self.title
+        {
+            let attributes:[String:Any] = [
+                NSFontAttributeName:UIFont.regular(size:kTitleFontSize),
+                NSForegroundColorAttributeName:UIColor(
+                    white:kTitleWhite, alpha:1)
+            ]
+            
+            let titleSeparator:String = "\(kSubsenseSeparator)\(title)"
+            let titleString:NSAttributedString = NSAttributedString(
+                string:titleSeparator,
+                attributes:attributes)
+            
+            mutableString.append(titleString)
+        }
+        
+        if let examples:[String] = self.examples
+        {
+            let attributes:[String:Any] = [
+                NSFontAttributeName:UIFont.regular(size:kExampleFontSize),
+                NSForegroundColorAttributeName:UIColor(
+                    white:kExampleWhite, alpha:1)
+            ]
+            
+            for example:String in examples
+            {
+                let exampleSeparator:String = "\(kExampleSeparator)\(example)"
+                let exampleString:NSAttributedString = NSAttributedString(
+                    string:exampleSeparator,
+                    attributes:attributes)
+                
+                mutableString.append(exampleString)
+            }
+        }
+        
+        return mutableString
     }
 }
