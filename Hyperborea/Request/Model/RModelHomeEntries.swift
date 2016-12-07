82a3eb04f8d97bd28ef04b5d0a360afe3ef18a36
vauxhall <zero.griffin@gmail.com>
2016-12-06 18:08:07 -0600

    resource lexical categories

--- Hyperborea/Request/Model/RModelHomeEntries.swift
+++ Hyperborea/Request/Model/RModelHomeEntries.swift
@@ -3,13 +3,23 @@ import Foundation
 class RModelHomeEntries:RModel
 {
     let items:[RModelHomeEntriesItem]
+    private let kKeyResults:String = "results"
+    private let kFontSizeCategory:CGFloat = 14
+    private let kMaxWidth:CGFloat = 2000
+    private let kMaxHeight:CGFloat = 20
     
     required init(json:Any)
     {
-        print(json)
+        var items:[RModelHomeEntriesItem] = []
         
-        items = [
-        ]
+        let jsonMap:[String:Any]? = json as? [String:Any]
+        
+        if let jsonResults:[Any] = jsonMap?[kKeyResults] as? [Any]
+        {
+            
+        }
+        
+        self.items = items
         
         super.init()
     }
---
+++ Hyperborea/Request/Model/RModelHomeEntriesItemNoun.swift
@@ -0,0 +1,9 @@
+//
+//  RModelHomeEntriesItemNoun.swift
+//  Hyperborea
+//
+//  Created by zero on 12/6/16.
+//  Copyright © 2016 iturbide. All rights reserved.
+//
+
+import Foundation
