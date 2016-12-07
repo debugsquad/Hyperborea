3d8429fb9a1b81c7093ed8de0c7f0967bcf35c95
vauxhall <zero.griffin@gmail.com>
2016-12-06 18:14:31 -0600

    loading lexical map

--- Hyperborea/Request/Model/Entries/RModelHomeEntriesFactory.swift
+++ Hyperborea/Request/Model/Entries/RModelHomeEntriesFactory.swift
@@ -3,9 +3,38 @@ import Foundation
 class RModelHomeEntriesFactory
 {
     static let sharedInstance:RModelHomeEntriesFactory = RModelHomeEntriesFactory()
+    private let lexicalMap:[String:String]?
+    private let kResourceName:String = "ResourceLexicalCategory"
+    private let kResourceExtension:String = "plist"
+    private let kEmpty:String = ""
     
     private init()
     {
+        guard
+            
+            let resourceUrl:URL = Bundle.main.url(
+                forResource:kResourceName,
+                withExtension:kResourceExtension)
+            
+        else
+        {
+            lexicalMap = nil
+            
+            return
+        }
         
+        guard
+            
+            let lexicalDictionary:NSDictionary = NSDictionary(
+                contentsOf:resourceUrl)
+            
+        else
+        {
+            lexicalMap = nil
+            
+            return
+        }
+        
+        lexicalMap = lexicalDictionary as? [String:String]
     }
 }
