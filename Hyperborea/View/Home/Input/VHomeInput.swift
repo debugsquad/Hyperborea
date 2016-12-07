f3914c1c2e543055020f66a5654f8354d792e022
vauxhall <zero.griffin@gmail.com>
2016-12-07 11:56:25 -0600

    added blur on input view

--- Hyperborea/View/Home/Input/VHomeInput.swift
+++ Hyperborea/View/Home/Input/VHomeInput.swift
@@ -16,6 +16,12 @@ class VHomeInput:UIView
         
         let barHeight:CGFloat = VParent.kBarHeight
         
+        let blurEffect:UIBlurEffect = UIBlurEffect(style:UIBlurEffectStyle.light)
+        let visualEffect:UIVisualEffectView = UIVisualEffectView(effect:blurEffect)
+        visualEffect.translatesAutoresizingMaskIntoConstraints = false
+        visualEffect.clipsToBounds = true
+        visualEffect.isUserInteractionEnabled = false
+        
         let viewText:VHomeInputText = VHomeInputText(controller:controller)
         self.viewText = viewText
         
@@ -24,6 +30,39 @@ class VHomeInput:UIView
         
         addSubview(viewPlaceholder)
         addSubview(viewText)
+        
+        let layoutVisualEffectTop:NSLayoutConstraint = NSLayoutConstraint(
+            item:visualEffect,
+            attribute:NSLayoutAttribute.top,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.top,
+            multiplier:1,
+            constant:0)
+        let layoutVisualEffectBottom:NSLayoutConstraint = NSLayoutConstraint(
+            item:visualEffect,
+            attribute:NSLayoutAttribute.bottom,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.bottom,
+            multiplier:1,
+            constant:0)
+        let layoutVisualEffectLeft:NSLayoutConstraint = NSLayoutConstraint(
+            item:visualEffect,
+            attribute:NSLayoutAttribute.left,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.left,
+            multiplier:1,
+            constant:0)
+        let layoutVisualEffectRight:NSLayoutConstraint = NSLayoutConstraint(
+            item:visualEffect,
+            attribute:NSLayoutAttribute.right,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.right,
+            multiplier:1,
+            constant:0)
         
         let layoutViewTextTop:NSLayoutConstraint = NSLayoutConstraint(
             item:viewText,
@@ -92,6 +131,10 @@ class VHomeInput:UIView
             constant:0)
 
         addConstraints([
+            layoutVisualEffectTop,
+            layoutVisualEffectBottom,
+            layoutVisualEffectLeft,
+            layoutVisualEffectRight,
             layoutViewTextTop,
             layoutViewTextBottom,
             layoutViewTextLeft,
