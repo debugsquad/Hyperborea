0b1d7316cb34368ce4448de1bb4ad6ef5e74f489
vauxhall <zero.griffin@gmail.com>
2016-12-09 10:21:11 -0600

    added image view

--- Hyperborea/View/Main/VParentBar.swift
+++ Hyperborea/View/Main/VParentBar.swift
@@ -4,8 +4,11 @@ class VParentBar:UIView
 {
     private weak var controller:CParent!
     private weak var labelTitle:UILabel!
-    private let kTitleTop:CGFloat = 20
-    private let kTitleLeft:CGFloat = 20
+    private weak var imageView:UIImageView!
+    private let kContentTop:CGFloat = 20
+    private let kImageLeft:CGFloat = 10
+    private let kImageWidth:CGFloat = 40
+    private let kTitleWidth:CGFloat = 200
     
     convenience init(controller:CParent)
     {
@@ -13,6 +16,14 @@ class VParentBar:UIView
         clipsToBounds = true
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = UIColor.clear
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = #imageLiteral(resourceName: "assetHomeSearch")
+        self.imageView = imageView
         
         let labelTitle:UILabel = UILabel()
         labelTitle.isUserInteractionEnabled = false
@@ -24,23 +35,57 @@ class VParentBar:UIView
         self.labelTitle = labelTitle
 
         addSubview(labelTitle)
+        addSubview(imageView)
+        
+        let layoutImageViewLeft:NSLayoutConstraint = NSLayoutConstraint(
+            item:imageView,
+            attribute:NSLayoutAttribute.left,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.left,
+            multiplier:1,
+            constant:kImageLeft)
+        let layoutImageViewWidth:NSLayoutConstraint = NSLayoutConstraint(
+            item:imageView,
+            attribute:NSLayoutAttribute.width,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:nil,
+            attribute:NSLayoutAttribute.notAnAttribute,
+            multiplier:1,
+            constant:kImageWidth)
+        let layoutImageViewTop:NSLayoutConstraint = NSLayoutConstraint(
+            item:imageView,
+            attribute:NSLayoutAttribute.top,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.top,
+            multiplier:1,
+            constant:kContentTop)
+        let layoutImageViewBottom:NSLayoutConstraint = NSLayoutConstraint(
+            item:imageView,
+            attribute:NSLayoutAttribute.bottom,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.bottom,
+            multiplier:1,
+            constant:0)
         
         let layoutLabelTitleLeft:NSLayoutConstraint = NSLayoutConstraint(
             item:labelTitle,
             attribute:NSLayoutAttribute.left,
             relatedBy:NSLayoutRelation.equal,
-            toItem:self,
-            attribute:NSLayoutAttribute.left,
-            multiplier:1,
-            constant:kTitleLeft)
-        let layoutLabelTitleRight:NSLayoutConstraint = NSLayoutConstraint(
-            item:labelTitle,
-            attribute:NSLayoutAttribute.right,
-            relatedBy:NSLayoutRelation.equal,
-            toItem:self,
+            toItem:imageView,
             attribute:NSLayoutAttribute.right,
             multiplier:1,
             constant:0)
+        let layoutLabelTitleWidth:NSLayoutConstraint = NSLayoutConstraint(
+            item:labelTitle,
+            attribute:NSLayoutAttribute.width,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:nil,
+            attribute:NSLayoutAttribute.notAnAttribute,
+            multiplier:1,
+            constant:kTitleWidth)
         let layoutLabelTitleTop:NSLayoutConstraint = NSLayoutConstraint(
             item:labelTitle,
             attribute:NSLayoutAttribute.top,
@@ -48,7 +93,7 @@ class VParentBar:UIView
             toItem:self,
             attribute:NSLayoutAttribute.top,
             multiplier:1,
-            constant:kTitleTop)
+            constant:kContentTop)
         let layoutLabelTitleBottom:NSLayoutConstraint = NSLayoutConstraint(
             item:labelTitle,
             attribute:NSLayoutAttribute.bottom,
@@ -59,9 +104,13 @@ class VParentBar:UIView
             constant:0)
         
         addConstraints([
+            layoutImageViewLeft,
+            layoutImageViewWidth,
+            layoutImageViewTop,
+            layoutImageViewBottom,
             layoutLabelTitleTop,
             layoutLabelTitleLeft,
-            layoutLabelTitleRight,
+            layoutLabelTitleWidth,
             layoutLabelTitleBottom
             ])
     }
