15130f9203c5fbfd6bf4d58006955c4672369fe8
vauxhall <zero.griffin@gmail.com>
2016-12-08 18:28:37 -0600

    parsing entries

--- Hyperborea/Request/Model/Entries/Abstract/RModelHomeEntries.swift
+++ Hyperborea/Request/Model/Entries/Abstract/RModelHomeEntries.swift
@@ -2,59 +2,67 @@ import Foundation
 
 class RModelHomeEntries:RModel
 {
-    let items:[RModelHomeEntriesItem]
+    private(set) var items:[RModelHomeEntriesItem]
     private let kKeyResults:String = "results"
     private let kKeyLexicalEntries:String = "lexicalEntries"
     private let kKeyLexicalCategory:String = "lexicalCategory"
     
     required init(json:Any)
     {
-        var items:[RModelHomeEntriesItem] = []
+        items = []
         
         guard
             
             let jsonMap:[String:Any] = json as? [String:Any],
-            let jsonResults:[Any] = jsonMap[kKeyResults] as? [Any],
-            let jsonFirstResult:[String:Any] = jsonResults.first as? [String:Any],
-            let jsonLexicalEntries:[Any] = jsonFirstResult[kKeyLexicalEntries] as? [Any]
+            let jsonResults:[Any] = jsonMap[kKeyResults] as? [Any]
         
         else
         {
-            self.items = items
             super.init()
             
             return
         }
         
-        for jsonEntry:Any in jsonLexicalEntries
+        for jsonResult:Any in jsonResults
         {
             guard
             
-                let jsonEntryMap:[String:Any] = jsonEntry as? [String:Any],
-                let entryCategory:String = jsonEntryMap[kKeyLexicalCategory] as? String,
-                let entryType:RModelHomeEntriesItem.Type = RModelHomeEntriesFactory.sharedInstance.itemWidthLexical(
-                    category:entryCategory)
+                let jsonResultMap:[String:Any] = jsonResult as? [String:Any],
+                let jsonLexicalEntries:[Any] = jsonResultMap[kKeyLexicalEntries] as? [Any]
             
             else
             {
                 continue
             }
             
-            let entry:RModelHomeEntriesItem = entryType.init(json:jsonEntry)
-            items.append(entry)
+            for jsonEntry:Any in jsonLexicalEntries
+            {
+                guard
+                    
+                    let jsonEntryMap:[String:Any] = jsonEntry as? [String:Any],
+                    let entryCategory:String = jsonEntryMap[kKeyLexicalCategory] as? String,
+                    let entryType:RModelHomeEntriesItem.Type = RModelHomeEntriesFactory.sharedInstance.itemWidthLexical(
+                        category:entryCategory)
+                    
+                else
+                {
+                    continue
+                }
+                
+                let entry:RModelHomeEntriesItem = entryType.init(json:jsonEntry)
+                items.append(entry)
+            }
+            
+            let etymologies:RModelHomeEntriesEtymologies = RModelHomeEntriesEtymologies(
+                json:jsonLexicalEntries)
+            
+            if !etymologies.titles.isEmpty
+            {
+                let origin:RModelHomeEntriesItemOrigin = RModelHomeEntriesItemOrigin(
+                    etymologies:etymologies)
+                items.append(origin)
+            }
         }
-        
-        let etymologies:RModelHomeEntriesEtymologies = RModelHomeEntriesEtymologies(
-            json:jsonLexicalEntries)
-        
-        if !etymologies.titles.isEmpty
-        {
-            let origin:RModelHomeEntriesItemOrigin = RModelHomeEntriesItemOrigin(
-                etymologies:etymologies)
-            items.append(origin)
-        }
-        
-        self.items = items
         
         super.init()
     }
