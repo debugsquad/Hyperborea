e38bf09ccfa3bad7f144a84095e14da629b3c8e3
vauxhall <zero.griffin@gmail.com>
2016-12-09 15:41:27 -0600

    button dismiss and constraints

--- Hyperborea/View/HomeLanguage/VHomeLanguage.swift
+++ Hyperborea/View/HomeLanguage/VHomeLanguage.swift
@@ -9,6 +9,51 @@ class VHomeLanguage:VView
         super.init(controller:controller)
         backgroundColor = UIColor(white:0, alpha:0.5)
         self.controller = controller as? CHomeLanguage
+        
+        let buttonDismiss:UIButton = UIButton()
+        buttonDismiss.translatesAutoresizingMaskIntoConstraints = false
+        buttonDismiss.backgroundColor = UIColor.clear
+        
+        addSubview(buttonDismiss)
+        
+        let layoutButtonDismissTop:NSLayoutConstraint = NSLayoutConstraint(
+            item:buttonDismiss,
+            attribute:NSLayoutAttribute.top,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.top,
+            multiplier:1,
+            constant:0)
+        let layoutButtonDismissBottom:NSLayoutConstraint = NSLayoutConstraint(
+            item:buttonDismiss,
+            attribute:NSLayoutAttribute.bottom,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.bottom,
+            multiplier:1,
+            constant:0)
+        let layoutButtonDismissLeft:NSLayoutConstraint = NSLayoutConstraint(
+            item:buttonDismiss,
+            attribute:NSLayoutAttribute.left,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.left,
+            multiplier:1,
+            constant:0)
+        let layoutButtonDismissRight:NSLayoutConstraint = NSLayoutConstraint(
+            item:buttonDismiss,
+            attribute:NSLayoutAttribute.right,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.right,
+            multiplier:1,
+            constant:0)
+        
+        addConstraints([
+            layoutButtonDismissTop,
+            layoutButtonDismissBottom,
+            layoutButtonDismissLeft,
+            layoutButtonDismissRight])
     }
     
     required init?(coder:NSCoder)
