68b8bb6afa97b495e01f465b786717ff8bbcf89e
vauxhall <zero.griffin@gmail.com>
2016-12-09 18:37:23 -0600

    notification language changed

--- Hyperborea/Extensions/ExtensionNotification.swift
+++ Hyperborea/Extensions/ExtensionNotification.swift
@@ -3,4 +3,5 @@ import Foundation
 extension Notification
 {
     static let requestCancel:Notification.Name = Notification.Name("requestCancel")
+    static let languageChanged:Notification.Name = Notification.Name("languageChanged")
 }
