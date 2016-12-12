cae64a1ecce6236e4305fb7c3909b2b834604cef
vauxhall <zero.griffin@gmail.com>
2016-12-12 15:19:17 -0600

    item flux factory with status

--- Hyperborea/Model/MSessionFluxItem.swift
+++ Hyperborea/Model/MSessionFluxItem.swift
@@ -2,9 +2,37 @@ import UIKit
 
 class MSessionFluxItem
 {
+    class func factory(status:MSessionFlux.Status) -> MSessionFluxItem
+    {
+        let item:MSessionFluxItem
+        
+        switch status
+        {
+            case MSessionFlux.Status.full:
+            
+                item = MSessionFluxItemFull()
+                
+                break
+            
+            case MSessionFlux.Status.twoLeft:
+                
+                break
+            
+            case MSessionFlux.Status.oneLeft:
+            
+                break
+            
+            case MSessionFlux.Status.empty:
+            
+                break
+        }
+        
+        return item
+    }
+    
     let status:MSessionFlux.Status
     let image:UIImage
-
+    
     init(status:MSessionFlux.Status, image:UIImage)
     {
         self.status = status
