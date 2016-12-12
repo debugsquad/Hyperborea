8748887ce61d574dc0fb7bb610f803d034013042
vauxhall <zero.griffin@gmail.com>
2016-12-12 14:28:42 -0600

    view helper flux

---
+++ Hyperborea/View/Home/Helper/VHomeHelperFlux.swift
@@ -0,0 +1,64 @@
+import UIKit
+
+class VHomeHelperFlux:UIButton
+{
+    private weak var controller:CHome!
+    private weak var image:UIImageView!
+    
+    convenience init(controller:CHome)
+    {
+        self.init()
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        let image:UIImageView = UIImageView()
+        image.isUserInteractionEnabled = false
+        image.clipsToBounds = true
+        image.contentMode = UIViewContentMode.center
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = #imageLiteral(resourceName: "assetHomeFluxFull")
+        self.image = image
+        
+        addSubview(image)
+        
+        let layoutImageTop:NSLayoutConstraint = NSLayoutConstraint(
+            item:image,
+            attribute:NSLayoutAttribute.top,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.top,
+            multiplier:1,
+            constant:0)
+        let layoutImageBottom:NSLayoutConstraint = NSLayoutConstraint(
+            item:image,
+            attribute:NSLayoutAttribute.bottom,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.bottom,
+            multiplier:1,
+            constant:0)
+        let layoutImageLeft:NSLayoutConstraint = NSLayoutConstraint(
+            item:image,
+            attribute:NSLayoutAttribute.left,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.left,
+            multiplier:1,
+            constant:0)
+        let layoutImageRight:NSLayoutConstraint = NSLayoutConstraint(
+            item:image,
+            attribute:NSLayoutAttribute.right,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.right,
+            multiplier:1,
+            constant:0)
+        
+        addConstraints([
+            layoutImageTop,
+            layoutImageBottom,
+            layoutImageLeft,
+            layoutImageRight])
+    }
+}
