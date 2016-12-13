72094294c59d3ed1e91aaa6ffc3db19d297864cd
vauxhall <zero.griffin@gmail.com>
2016-12-12 19:34:19 -0600

    factory next status

--- Hyperborea/Model/MSessionFluxEmpty.swift
+++ Hyperborea/Model/MSessionFluxEmpty.swift
@@ -3,18 +3,21 @@ import UIKit
 class MSessionFluxEmpty:MSessionFlux
 {
     private let kStatus:MSessionFlux.Status = MSessionFlux.Status.empty
+    private let kNextStatus:MSessionFlux.Status = MSessionFlux.Status.full
     private let kSearchAvailable:Bool = false
     
     override init()
     {
         super.init(
             status:kStatus,
+            nextStatus:kNextStatus,
             image:#imageLiteral(resourceName: "assetHomeFluxEmpty"),
             searchAvailable:kSearchAvailable)
     }
     
     override init(
         status:MSessionFlux.Status,
+        nextStatus:MSessionFlux.Status,
         image:UIImage,
         searchAvailable:Bool)
     {
--- Hyperborea/Model/MSessionFluxOneLeft.swift
+++ Hyperborea/Model/MSessionFluxOneLeft.swift
@@ -3,18 +3,21 @@ import UIKit
 class MSessionFluxOneLeft:MSessionFlux
 {
     private let kStatus:MSessionFlux.Status = MSessionFlux.Status.oneLeft
+    private let kNextStatus:MSessionFlux.Status = MSessionFlux.Status.empty
     private let kSearchAvailable:Bool = true
     
     override init()
     {
         super.init(
             status:kStatus,
+            nextStatus:kNextStatus,
             image:#imageLiteral(resourceName: "assetHomeFluxOneLeft"),
             searchAvailable:kSearchAvailable)
     }
     
     override init(
         status:MSessionFlux.Status,
+        nextStatus:MSessionFlux.Status,
         image:UIImage,
         searchAvailable:Bool)
     {
