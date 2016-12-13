d2f5c5d2b43fc1699dda45f19c5f1f1370d0716b
vauxhall <zero.griffin@gmail.com>
2016-12-13 10:02:19 -0600

    view froob constraints

--- Hyperborea/View/HomeFroob/VHomeFroob.swift
+++ Hyperborea/View/HomeFroob/VHomeFroob.swift
@@ -3,15 +3,122 @@ import UIKit
 class VHomeFroob:VView
 {
     private weak var controller:CHomeFroob!
+    private weak var layoutBaseLeft:NSLayoutConstraint!
+    private weak var layoutBaseTop:NSLayoutConstraint!
+    private let kBaseWidth:CGFloat = 300
+    private let kBaseHeight:CGFloat = 280
+    private let kButtonHeight:CGFloat = 42
+    private let kCornerRadius:CGFloat = 12
     
     override init(controller:CController)
     {
         super.init(controller:controller)
         self.controller = controller as? CHomeFroob
+        
+        let blurEffect:UIBlurEffect = UIBlurEffect(style:UIBlurEffectStyle.light)
+        let visualEffect:UIVisualEffectView = UIVisualEffectView(effect:blurEffect)
+        visualEffect.isUserInteractionEnabled = false
+        visualEffect.translatesAutoresizingMaskIntoConstraints = false
+        visualEffect.clipsToBounds = true
+        
+        let baseButton:UIButton = UIButton()
+        baseButton.clipsToBounds = true
+        baseButton.backgroundColor = UIColor.clear
+        baseButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        let baseView:UIView = UIView()
+        baseView.translatesAutoresizingMaskIntoConstraints = false
+        baseView.clipsToBounds = true
+        baseView.backgroundColor = UIColor.white
+        baseView.layer.borderWidth = 1
+        baseView.layer.borderColor = UIColor(white:0, alpha:0.5).cgColor
+        baseView.layer.cornerRadius = kCornerRadius
+        
+        addSubview(visualEffect)
+        addSubview(baseButton)
+        addSubview(baseView)
+        
+        let layoutVisualEffectTop:NSLayoutConstraint = NSLayoutConstraint(
+            item:visualEffect,
+            attribute:NSLayoutAttribute.top,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.top,
+            multiplier:1,
+            constant:0)
+        let layoutVisualEffectBottom:NSLayoutConstraint = NSLayoutConstraint(
+            item:visualEffect,
+            attribute:NSLayoutAttribute.bottom,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.bottom,
+            multiplier:1,
+            constant:0)
+        let layoutVisualEffectLeft:NSLayoutConstraint = NSLayoutConstraint(
+            item:visualEffect,
+            attribute:NSLayoutAttribute.left,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.left,
+            multiplier:1,
+            constant:0)
+        let layoutVisualEffectRight:NSLayoutConstraint = NSLayoutConstraint(
+            item:visualEffect,
+            attribute:NSLayoutAttribute.right,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.right,
+            multiplier:1,
+            constant:0)
+        
+        let layoutBaseButtonTop:NSLayoutConstraint = NSLayoutConstraint(
+            item:visualEffect,
+            attribute:NSLayoutAttribute.top,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.top,
+            multiplier:1,
+            constant:0)
+        let layoutBaseButtonBottom:NSLayoutConstraint = NSLayoutConstraint(
+            item:visualEffect,
+            attribute:NSLayoutAttribute.bottom,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.bottom,
+            multiplier:1,
+            constant:0)
+        let layoutBaseButtonLeft:NSLayoutConstraint = NSLayoutConstraint(
+            item:visualEffect,
+            attribute:NSLayoutAttribute.left,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.left,
+            multiplier:1,
+            constant:0)
+        let layoutBaseButtonRight:NSLayoutConstraint = NSLayoutConstraint(
+            item:visualEffect,
+            attribute:NSLayoutAttribute.right,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.right,
+            multiplier:1,
+            constant:0)
+        
+        addConstraints([
+            layoutVisualEffectTop,
+            layoutVisualEffectBottom,
+            layoutVisualEffectLeft,
+            layoutVisualEffectRight,
+            layoutBaseButtonTop,
+            layoutBaseButtonBottom,
+            layoutBaseButtonLeft,
+            layoutBaseButtonRight])
     }
     
     required init?(coder:NSCoder)
     {
         fatalError()
     }
+    
+    
 }
