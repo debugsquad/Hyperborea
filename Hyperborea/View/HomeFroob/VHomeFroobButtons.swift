5695b03a884d0a80c98171372f6ca0b825194ba0
vauxhall <zero.griffin@gmail.com>
2016-12-13 16:07:32 -0600

    wording

--- Hyperborea/View/HomeFroob/VHomeFroobButtons.swift
+++ Hyperborea/View/HomeFroob/VHomeFroobButtons.swift
@@ -11,5 +11,21 @@ class VHomeFroobButtons:UIView
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = UIColor.genericBorder
         self.controller = controller
+        
+        let buttonStore:UIButton = UIButton()
+        buttonStore.translatesAutoresizingMaskIntoConstraints = false
+        buttonStore.clipsToBounds = true
+        buttonStore.backgroundColor = UIColor.white
+        buttonStore.setTitle(
+            NSLocalizedString("VHomeFroobButtons_buttonStore", comment:""),
+            for:UIControlState.normal)
+        buttonStore.setTitleColor(
+            UIColor.genericBlue,
+            for:UIControlState.normal)
+        buttonStore.setTitleColor(
+            UIColor(white:0, alpha:0.2),
+            for:UIControlState.highlighted)
+        buttonStore.titleLabel!.font = UIFont.medium(size:14)
+        addSubview(buttonStore)
     }
 }
