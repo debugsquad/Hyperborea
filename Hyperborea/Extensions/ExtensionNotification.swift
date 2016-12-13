280b702ff5eda2a9abdbf14991e34cc8cd7f31ca
vauxhall <zero.griffin@gmail.com>
2016-12-12 19:27:18 -0600

    notification flux update

--- Hyperborea/Extensions/ExtensionNotification.swift
+++ Hyperborea/Extensions/ExtensionNotification.swift
@@ -5,4 +5,5 @@ extension Notification
     static let sessionLoaded:Notification.Name = Notification.Name("sessionLoaded")
     static let requestCancel:Notification.Name = Notification.Name("requestCancel")
     static let languageChanged:Notification.Name = Notification.Name("languageChanged")
+    static let fluxUpdate:Notification.Name = Notification.Name("fluxUpdate")
 }
