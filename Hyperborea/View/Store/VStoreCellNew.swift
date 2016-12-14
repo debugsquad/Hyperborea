4a1bf1d98853734134f1ede1fb642bf2752349ad
vauxhall <zero.griffin@gmail.com>
2016-12-14 10:09:24 -0600

    view store cell new

--- Hyperborea/View/Store/VStoreCellNew.swift
+++ Hyperborea/View/Store/VStoreCellNew.swift
@@ -1,9 +1,6 @@
-//
-//  VStoreCellNew.swift
-//  Hyperborea
-//
-//  Created by zero on 12/14/16.
-//  Copyright © 2016 iturbide. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class VStoreCellNew:VStoreCell
+{
+    
+}
