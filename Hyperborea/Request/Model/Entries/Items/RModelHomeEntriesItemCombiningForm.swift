df78a18e29b494652b09e3e1a9c86275d35deb44
vauxhall <zero.griffin@gmail.com>
2016-12-14 17:39:18 -0600

    combining form

---
+++ Hyperborea/Request/Model/Entries/Items/RModelHomeEntriesItemCombiningForm.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+class RModelHomeEntriesItemCombiningForm:RModelHomeEntriesItem
+{
+    private let kSelectable:Bool = false
+    
+    required init(json:Any)
+    {
+        let attributes:[String:Any] = [
+            NSFontAttributeName:UIFont.medium(
+                size:RModelHomeEntriesItem.kTitleFontSize),
+            NSForegroundColorAttributeName:UIColor(
+                white:RModelHomeEntriesItem.kTitleWhite,
+                alpha:1)
+        ]
+        
+        let title:String = NSLocalizedString("RModelHomeEntriesItemCombiningForm_title", comment:"")
+        let senses:RModelHomeEntriesSenses = RModelHomeEntriesSenses(json:json)
+        let tense:RModelHomeEntriesTense = RModelHomeEntriesTense(json:json)
+        let titleString:NSAttributedString = NSAttributedString(
+            string:title,
+            attributes:attributes)
+        let sensesString:NSAttributedString = senses.attributedString()
+        let tenseString:NSAttributedString = tense.attributedString()
+        
+        let mutableString:NSMutableAttributedString = NSMutableAttributedString()
+        mutableString.append(titleString)
+        mutableString.append(tenseString)
+        mutableString.append(sensesString)
+        
+        let reusableIdentifier:String = VHomeWordsCellEntry.reusableIdentifier
+        
+        super.init(
+            attributedString:mutableString,
+            reusableIdentifier:reusableIdentifier,
+            selectable:kSelectable)
+    }
+    
+    override init(
+        attributedString:NSAttributedString,
+        reusableIdentifier:String,
+        selectable:Bool)
+    {
+        fatalError()
+    }
+}
