745f4afff2ef4d16615dcffdd31ba1bbdc6afbee
vauxhall <zero.griffin@gmail.com>
2016-12-14 10:09:40 -0600

    view store cell purchasing

--- Hyperborea/View/Store/VStoreCellPurchasing.swift
+++ Hyperborea/View/Store/VStoreCellPurchasing.swift
@@ -1,9 +1,6 @@
-//
-//  VStoreCellPurchasing.swift
-//  Hyperborea
-//
-//  Created by zero on 12/14/16.
-//  Copyright © 2016 iturbide. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class VStoreCellPurchasing:VStoreCell
+{
+    
+}
