2d9ad2f490366194739ed370baf2cf4f790803d7
vauxhall <zero.griffin@gmail.com>
2016-12-14 19:38:15 -0600

    credentials 3

---
+++ Hyperborea/Request/Credentials/RCredentialsItem3.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+class RCredentialsItem3:RCredentialsItem
+{
+    private let kAppId:String = "7b3c630e"
+    private let kAppKey:String = "e1497641b34f4a3fefc31f0c76839bf8"
+    
+    override init()
+    {
+        super.init(appId:kAppId, appKey:kAppKey)
+    }
+    
+    override init(appId:String, appKey:String)
+    {
+        fatalError()
+    }
+}
