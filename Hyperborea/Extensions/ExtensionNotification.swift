5d8ec0497cae9b00c07e35fb9111df961852ab84
vauxhall <zero.griffin@gmail.com>
2017-03-28 12:55:47 -0600

    restart project

--- Hyperborea/Extensions/ExtensionNotification.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension Notification
-{
-    static let sessionLoaded:Notification.Name = Notification.Name("sessionLoaded")
-    static let requestCancel:Notification.Name = Notification.Name("requestCancel")
-    static let languageChanged:Notification.Name = Notification.Name("languageChanged")
-    static let fluxUpdate:Notification.Name = Notification.Name("fluxUpdate")
-}
