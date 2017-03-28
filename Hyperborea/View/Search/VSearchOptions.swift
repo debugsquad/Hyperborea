943763b90ccabc01718b3ccd058cf8bd5c421152
vauxhall <zero.griffin@gmail.com>
2017-03-28 15:26:05 -0600

    options collection view

--- Hyperborea/View/Search/VSearchOptions.swift
+++ Hyperborea/View/Search/VSearchOptions.swift
@@ -1,8 +1,11 @@
 import UIKit
 
-class VSearchOptions:UIView
+class VSearchOptions:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
     private weak var controller:CSearch!
+    private weak var collectionView:VCollection!
+    private let kInterItem:CGFloat = 3
+    private let kCellWidth:CGFloat = 60
     
     init(controller:CSearch)
     {
@@ -10,6 +13,32 @@ class VSearchOptions:UIView
         clipsToBounds = true
         translatesAutoresizingMaskIntoConstraints = false
         self.controller = controller
+        
+        let collectionView:VCollection = VCollection()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.registerCell(cell:VSearchOptionsCell.self)
+        collectionView.isScrollEnabled = false
+        collectionView.bounces = false
+        self.collectionView = collectionView
+        
+        if let flow:VCollectionFlow = collectionView.collectionViewLayout as? VCollectionFlow
+        {
+            flow.scrollDirection = UICollectionViewScrollDirection.horizontal
+            flow.minimumLineSpacing = kInterItem
+            flow.minimumInteritemSpacing = kInterItem
+            flow.sectionInset = UIEdgeInsets(
+                top:kInterItem,
+                left:kInterItem,
+                bottom:kInterItem,
+                right:kInterItem)
+        }
+        
+        addSubview(collectionView)
+        
+        NSLayoutConstraint.equals(
+            view:collectionView,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
@@ -17,5 +46,36 @@ class VSearchOptions:UIView
         return nil
     }
     
+    //MARK: collectionView delegate
     
+    func collectionView(_ collectionView:UICollectionView, layout collectionViewLayout:UICollectionViewLayout, sizeForItemAt indexPath:IndexPath) -> CGSize
+    {
+        let height:CGFloat = collectionView.bounds.maxY
+        let remainHeight:CGFloat = height - (kInterItem + kInterItem)
+        let size:CGSize = CGSize(
+            width:kCellWidth,
+            height:remainHeight)
+        
+        return size
+    }
+    
+    func numberOfSections(in collectionView:UICollectionView) -> Int
+    {
+        return 1
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, numberOfItemsInSection section:Int) -> Int
+    {
+        return 3
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, cellForItemAt indexPath:IndexPath) -> UICollectionViewCell
+    {
+        let cell:VSearchOptionsCell = collectionView.dequeueReusableCell(
+            withReuseIdentifier:
+            VSearchOptionsCell.reusableIdentifier,
+            for:indexPath) as! VSearchOptionsCell
+        
+        return cell
+    }
 }
