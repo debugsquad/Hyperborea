0d4b582ac13ebfaa9e94dc771d06147b91bcbdc6
vauxhall <zero.griffin@gmail.com>
2017-03-28 17:43:44 -0600

    mock up items with lorem ipsum

--- Hyperborea/Model/Search/MSearchResults.swift
+++ Hyperborea/Model/Search/MSearchResults.swift
@@ -2,5 +2,20 @@ import Foundation
 
 class MSearchResults
 {
-    let items:[]
+    let items:[MSearchResultItem]
+    
+    init()
+    {
+        let loremIpsum:String = "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+        let loremArray:[String] = loremIpsum.components(separatedBy:"\n")
+        var items:[MSearchResultItem] = []
+        
+        for loremItem:String in loremArray
+        {
+            let item:MSearchResultItem = MSearchResultItem(name:loremItem)
+            items.append(item)
+        }
+        
+        self.items = items
+    }
 }
