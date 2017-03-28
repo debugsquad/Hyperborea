20627461f3436bb5b2cbdfdb953fb57109882a63
vauxhall <zero.griffin@gmail.com>
2017-03-28 14:08:52 -0600

    search bar layout

--- Hyperborea/View/Search/VSearch.swift
+++ Hyperborea/View/Search/VSearch.swift
@@ -4,12 +4,30 @@ class VSearch:VView
 {
     private weak var controller:CSearch!
     private weak var viewBar:VSearchBar!
+    private weak var layoutBarHeight:NSLayoutConstraint!
+    private let kBarMaxHeight:CGFloat = 100
     
     override init(controller:CController)
     {
         super.init(controller:controller)
         backgroundColor = UIColor(red:0.84, green:0.86, blue:0.9, alpha:1)
         self.controller = controller as? CSearch
+        
+        let viewBar:VSearchBar = VSearchBar(
+            controller:self.controller)
+        self.viewBar = viewBar
+        
+        addSubview(viewBar)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewBar,
+            toView:self)
+        layoutBarHeight = NSLayoutConstraint.height(
+            view:viewBar,
+            constant:kBarMaxHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewBar,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
--- Hyperborea/View/Search/VSearchBar.swift
+++ Hyperborea/View/Search/VSearchBar.swift
@@ -2,5 +2,19 @@ import UIKit
 
 class VSearchBar:UIView
 {
+    private weak var controller:CSearch!
     
+    init(controller:CSearch)
+    {
+        super.init(frame:CGRect.zero)
+        clipsToBounds = true
+        backgroundColor = UIColor.white
+        translatesAutoresizingMaskIntoConstraints = false
+        self.controller = controller
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
