bfd0be30f6d7d9ddea558af5497aa40842efb73b
vauxhall <zero.griffin@gmail.com>
2017-03-29 14:45:30 -0600

    content item definition

--- Hyperborea/Model/Search/MSearchContentMode.swift
+++ Hyperborea/Model/Search/MSearchContentMode.swift
@@ -1,9 +1,19 @@
-import Foundation
+import UIKit
 
 class MSearchContentMode
 {
+    let items:[MSearchContentModeItem]
+    private let kMarginHorizontal:CGFloat = 15
+    private let kFontSize:CGFloat = 14
+    
     init()
     {
+        let item
+        
+        
+        let attributes:[String:AnyObject] = [
+            NSFontAttributeName:UIFont.bold(size:kFontSize)]
+        
         
     }
 }
---
+++ Hyperborea/Model/Search/MSearchContentModeItemDefinition.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class MSearchContentModeItemDefinition:MSearchContentModeItem
+{
+    init()
+    {
+        let rawName:String = NSLocalizedString("MSearchContentModeItemDefinition_name", comment:"")
+        
+        super.init(rawName:rawName)
+    }
+}
