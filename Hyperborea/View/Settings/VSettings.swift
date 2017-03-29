b05c36553d6f1208e47719239fff05bb46d717e7
vauxhall <zero.griffin@gmail.com>
2017-03-29 17:17:21 -0600

    view bar

--- Hyperborea/View/Settings/VSettings.swift
+++ Hyperborea/View/Settings/VSettings.swift
@@ -3,11 +3,30 @@ import UIKit
 class VSettings:VView
 {
     private weak var controller:CSettings!
+    private weak var viewBar:VSettingsBar!
+    private weak var layoutBarHeight:NSLayoutConstraint!
+    private let kMaxBarHeight:CGFloat = 200
     
     override init(controller:CController)
     {
         super.init(controller:controller)
         self.controller = controller as? CSettings
+        
+        let viewBar:VSettingsBar = VSettingsBar(
+            controller:self.controller)
+        self.viewBar = viewBar
+        
+        addSubview(viewBar)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewBar,
+            toView:self)
+        layoutBarHeight = NSLayoutConstraint.height(
+            view:viewBar,
+            constant:kMaxBarHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewBar,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
--- Hyperborea/View/Settings/VSettingsBar.swift
+++ Hyperborea/View/Settings/VSettingsBar.swift
@@ -2,5 +2,19 @@ import UIKit
 
 class VSettingsBar:UIView
 {
+    private weak var controller:CSettings!
     
+    init(controller:CSettings)
+    {
+        super.init(frame:CGRect.zero)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        translatesAutoresizingMaskIntoConstraints = false
+        self.controller = controller
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
