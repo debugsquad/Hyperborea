8879544f89c839c5f5dd97d4157dac67c41b8c0b
vauxhall <zero.griffin@gmail.com>
2017-03-29 09:17:10 -0600

    attributed string and cell width

--- Hyperborea/Model/Search/MSearchResults.swift
+++ Hyperborea/Model/Search/MSearchResults.swift
@@ -4,16 +4,45 @@ class MSearchResults
 {
     let items:[MSearchResultsItem]
     private let kMarginHorizontal:CGFloat = 30
+    private let kFontSize:CGFloat = 14
+    private let kCellMaxWidth:CGFloat = 250
+    private let kCellMaxHeight:CGFloat = 30
+    private let kCellCompareWidth:CGFloat = 1000
     
     init()
     {
         let loremIpsum:String = "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
         let loremArray:[String] = loremIpsum.components(separatedBy:"\n")
         var items:[MSearchResultsItem] = []
+        let attributes:[String:AnyObject] = [
+            NSFontAttributeName:UIFont.regular(size:kFontSize)]
+        let maxSize:CGSize = CGSize(
+            width:kCellCompareWidth,
+            height:kCellMaxHeight)
+        let drawingOptions:NSStringDrawingOptions = NSStringDrawingOptions([
+            NSStringDrawingOptions.usesFontLeading,
+            NSStringDrawingOptions.usesLineFragmentOrigin])
         
         for loremItem:String in loremArray
         {
-            let item:MSearchResultsItem = MSearchResultsItem(name:loremItem)
+            let attributedString:NSAttributedString = NSAttributedString(
+                string:loremItem,
+                attributes:attributes)
+            let stringRect:CGRect = attributedString.boundingRect(
+                with:maxSize,
+                options:drawingOptions,
+                context:nil)
+            let stringWidth:CGFloat = ceil(stringRect.size.width)
+            var cellWidth:CGFloat = stringWidth + kMarginHorizontal
+            
+            if cellWidth > kCellMaxWidth
+            {
+                cellWidth = kCellMaxWidth
+            }
+            
+            let item:MSearchResultsItem = MSearchResultsItem(
+                name:attributedString,
+                cellWidth:cellWidth)
             items.append(item)
         }
         
