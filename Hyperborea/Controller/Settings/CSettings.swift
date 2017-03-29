86f5d54a3b129e4031b3a5fb4fdf3492f559fc0f
vauxhall <zero.griffin@gmail.com>
2017-03-29 17:14:03 -0600

    view settings bar

--- Hyperborea/Controller/Settings/CSettings.swift
+++ Hyperborea/Controller/Settings/CSettings.swift
@@ -2,5 +2,14 @@ import UIKit
 
 class CSettings:CController
 {
+    private weak var viewSettings:VSettings!
+    
+    override func loadView()
+    {
+        let viewSettings:VSettings = VSettings(controller:self)
+        self.viewSettings = viewSettings
+        view = viewSettings
+    }
+    
     
 }
