f9f5b07357d9394468eaa79766b882e2f28673ce
vauxhall <zero.griffin@gmail.com>
2017-03-31 16:03:40 -0600

    new url and credentials model

--- Hyperborea/Model/Main/MOxfordCredentials.swift
+++ Hyperborea/Model/Main/MOxfordCredentials.swift
@@ -2,35 +2,41 @@ import Foundation
 
 class MOxfordCredentials
 {
-    private let items:[RCredentialsItem]
+    private let credentials:[[String:String]]
     private let countItems:UInt32
+    private let kResourceName:String = "ResourceOxfordCredentials"
+    private let kResourceExtension:String = "plist"
     
     init()
     {
-        let item1:RCredentialsItem1 = RCredentialsItem1()
-        let item2:RCredentialsItem2 = RCredentialsItem2()
-        let item3:RCredentialsItem3 = RCredentialsItem3()
-        let item4:RCredentialsItem4 = RCredentialsItem4()
-        let item5:RCredentialsItem5 = RCredentialsItem5()
+        guard
+            
+            let resourceUrl:URL = Bundle.main.url(
+                forResource:kResourceName,
+                withExtension:kResourceExtension),
+            let credentialsArray:NSArray = NSArray(
+                contentsOf:resourceUrl),
+            let credentials:[[String:String]] = credentialsArray as? [[String:String]]
+            
+        else
+        {
+            self.credentials = [[:]]
+            self.countItems = 0
+            
+            return
+        }
         
-        items = [
-            item1,
-            item2,
-            item3,
-            item4,
-            item5
-        ]
-        
-        countItems = UInt32(items.count)
+        self.credentials = credentials
+        countItems = UInt32(credentials.count)
     }
     
     //MARK: public
     
-    func current() -> [String:String]
+    func credentialHeaders() -> [String:String]
     {
         let random:Int = Int(arc4random_uniform(countItems))
-        let item:RCredentialsItem = items[random]
+        let headers:[String:String] = credentials[random]
         
-        return item.credentials
+        return headers
     }
 }
--- Hyperborea/Model/Main/MUrls.swift
+++ Hyperborea/Model/Main/MUrls.swift
@@ -1,9 +1,63 @@
-//
-//  MUrls.swift
-//  Hyperborea
-//
-//  Created by zero on 3/31/17.
-//  Copyright © 2017 iturbide. All rights reserved.
-//
-
 import Foundation
+
+class MUrls
+{
+    enum Link:String
+    {
+        case share = "linkShare"
+        case review = "linkReview"
+    }
+    
+    enum Host:String
+    {
+        case hostOxford = "hostOxford"
+    }
+    
+    enum EndPoint:String
+    {
+        case oxfordSearch = "oxfordSearch"
+        case oxfordEntries = "oxfordEntries"
+    }
+    
+    private let urlMap:[String:String]
+    private let kResourceName:String = "ResourceUrl"
+    private let kResourceExtension:String = "plist"
+    
+    init()
+    {
+        guard
+            
+            let resourceUrl:URL = Bundle.main.url(
+                forResource:kResourceName,
+                withExtension:kResourceExtension),
+            let urlDictionary:NSDictionary = NSDictionary(
+                contentsOf:resourceUrl),
+            let urlMap:[String:String] = urlDictionary as? [String:String]
+            
+        else
+        {
+            self.urlMap = [:]
+            
+            return
+        }
+        
+        self.urlMap = urlMap
+    }
+    
+    //MARK: public
+    
+    func urlHost(host:Host) -> String?
+    {
+        return urlMap[host.rawValue]
+    }
+    
+    func urlEnpoint(endPoint:EndPoint) -> String?
+    {
+        return urlMap[endPoint.rawValue]
+    }
+    
+    func urlLink(link:Link) -> String?
+    {
+        return urlMap[link.rawValue]
+    }
+}
