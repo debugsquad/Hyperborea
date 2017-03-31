cbdfcb9e939134ba46e8656033752fecd0d1a87f
vauxhall <zero.griffin@gmail.com>
2017-03-31 11:11:59 -0600

    loading session settings

--- Hyperborea/Data/DSettingsExtension.swift
+++ Hyperborea/Data/DSettingsExtension.swift
@@ -3,5 +3,30 @@ import CoreData
 
 extension DSettings
 {
+    enum Language:Int16
+    {
+        case english = 0
+        case spanish
+    }
     
+    //MARK: public
+    
+    func addTtl()
+    {
+        ttl += 1
+        DManager.sharedInstance?.save()
+    }
+    
+    func currentLanguage() -> Language
+    {
+        let current:Language = Language(rawValue:language)!
+        
+        return current
+    }
+    
+    func changeLanguage(language:Language)
+    {
+        self.language = language.rawValue
+        DManager.sharedInstance?.save()
+    }
 }
