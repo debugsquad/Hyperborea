841c30f52c59de0acd6270f5e45d73aa39cdbf9e
vauxhall <zero.griffin@gmail.com>
2017-04-04 10:32:40 -0500

    entry item particle

---
+++ Hyperborea/Model/Search/MSearchEntryItemParticle.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class MSearchEntryItemParticle:MSearchEntryItem
+{
+    private let kFontSize:CGFloat = 16
+    
+    required init?(json:Any)
+    {
+        let attributes:[String:Any] = [
+            NSFontAttributeName:UIFont.medium(size:kFontSize)]
+        
+        let title:String = NSLocalizedString("MSearchEntryItemParticle_title", comment:"")
+        let mutableString:NSMutableAttributedString = NSMutableAttributedString()
+        
+        let titleString:NSAttributedString = NSAttributedString(
+            string:title,
+            attributes:attributes)
+        mutableString.append(titleString)
+        
+        if let sensesString:NSAttributedString = MSearchEntrySenses.parse(json:json)
+        {
+            mutableString.append(sensesString)
+        }
+        
+        super.init(attributedString:mutableString)
+    }
+}
