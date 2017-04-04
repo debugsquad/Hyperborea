9c21cf25e61a8022369c6b02bfbfc8753281bffd
vauxhall <zero.griffin@gmail.com>
2017-04-04 10:16:55 -0500

    Entry item contraction

---
+++ Hyperborea/Model/Search/MSearchEntryItemContraction.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class MSearchEntryItemContraction:MSearchEntryItem
+{
+    private let kFontSize:CGFloat = 16
+    
+    required init?(json:Any)
+    {
+        let attributes:[String:Any] = [
+            NSFontAttributeName:UIFont.medium(size:kFontSize)]
+        
+        let title:String = NSLocalizedString("MSearchEntryItemContraction_title", comment:"")
+        let mutableString:NSMutableAttributedString = NSMutableAttributedString()
+        
+        let titleString:NSAttributedString = NSAttributedString(
+            string:title,
+            attributes:attributes)
+        mutableString.append(titleString)
+        
+        if let sensesString:NSAttributedString = MSearchEntrySenses.parse(json:json)
+        {
+            mutableString.append(sensesString)
+        }
+        
+        super.init(attributedString:mutableString)
+    }
+}
