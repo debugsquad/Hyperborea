d0210ac9014be8c9f12937969310b820cc4237fe
vauxhall <zero.griffin@gmail.com>
2017-04-03 20:10:47 -0500

    entry tense

---
+++ Hyperborea/Model/Search/MSearchEntryItemVerb.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+class MSearchEntryItemVerb:MSearchEntryItem
+{
+    private let kFontSize:CGFloat = 16
+    
+    required init?(json:Any)
+    {
+        let attributes:[String:Any] = [
+            NSFontAttributeName:UIFont.medium(size:kFontSize)]
+        
+        let title:String = NSLocalizedString("MSearchEntryItemNoun_title", comment:"")
+        let mutableString:NSMutableAttributedString = NSMutableAttributedString()
+        
+        let titleString:NSAttributedString = NSAttributedString(
+            string:title,
+            attributes:attributes)
+        mutableString.append(titleString)
+        
+        if let numberString:NSAttributedString = MSearchEntryNumber.parse(json:json)
+        {
+            mutableString.append(numberString)
+        }
+        
+        if let sensesString:NSAttributedString = MSearchEntrySenses.parse(json:json)
+        {
+            mutableString.append(sensesString)
+        }
+        
+        super.init(attributedString:mutableString)
+    }
+}
---
+++ Hyperborea/Model/Search/MSearchEntryTense.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class MSearchEntryTense
+{
+    class func parse(json:Any) -> NSAttributedString?
+    {
+        
+    }
+}
