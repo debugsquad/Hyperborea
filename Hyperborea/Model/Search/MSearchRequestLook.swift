a3259cf994a263d7ecb899a92949e56f4649575d
vauxhall <zero.griffin@gmail.com>
2017-04-05 10:24:02 -0500

    refactor model search look

--- Hyperborea/Model/Search/MSearchRequestLook.swift
+++ Hyperborea/Model/Search/MSearchRequestLook.swift
@@ -1,43 +1,18 @@
 import Foundation
 
-class MSearchRequestLook
+class MSearchRequestLook:MSearchRequest
 {
     private weak var controller:CSearch?
-    private weak var task:URLSessionTask?
-    private let kMethod:String = "GET"
-    private let kNetworkServiceType:URLRequest.NetworkServiceType = URLRequest.NetworkServiceType.default
-    private let kCachePolicy:URLRequest.CachePolicy = URLRequest.CachePolicy.reloadIgnoringLocalCacheData
-    private let kResponseDisposition:URLSession.ResponseDisposition = URLSession.ResponseDisposition.allow
-    private let kTimeOutData:TimeInterval = 15
-    private let kCellularAccess:Bool = true
-    private let kDiscretionary:Bool = true
     
     @discardableResult init(controller:CSearch, query:String)
     {
         self.controller = controller
+        super.init()
         
         DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
         {
-            NotificationCenter.default.addObserver(
-                self,
-                selector:#selector(self.notifiedCancelRequests(sender:)),
-                name:Notification.cancelRequests,
-                object:nil)
-            
             self.asyncLookQuery(query:query)
         }
-    }
-    
-    deinit
-    {
-        NotificationCenter.default.removeObserver(self)
-    }
-    
-    //MARK: notified
-    
-    @objc func notifiedCancelRequests(sender notification:Notification)
-    {
-        task?.cancel()
     }
     
     //MARK: private
@@ -56,97 +31,85 @@ class MSearchRequestLook
         }
         
         let urlString:String = "\(urlHost)/\(urlEndPoint)/\(languageCode)?q=\(query)"
-        
-        #if DEBUG
-            
-            print(urlString)
-            
-        #endif
+        let headers:[String:String] = MSession.sharedInstance.modelOxfordCredentials.credentialHeaders()
         
         guard
-            
-            let url:URL = URL(string:urlString)
+        
+            let request:URLRequest = request(
+                urlString:urlString,
+                headers:headers)
             
         else
         {
             return
         }
         
-        let configuration:URLSessionConfiguration = URLSessionConfiguration.ephemeral
-        configuration.allowsCellularAccess = kCellularAccess
-        configuration.timeoutIntervalForRequest = kTimeOutData
-        configuration.timeoutIntervalForResource = kTimeOutData
-        configuration.isDiscretionary = kDiscretionary
-        configuration.networkServiceType = kNetworkServiceType
-        configuration.requestCachePolicy = kCachePolicy
-        
-        let headers:[String:String] = MSession.sharedInstance.modelOxfordCredentials.credentialHeaders()
-        let urlMutableRequest:NSMutableURLRequest = NSMutableURLRequest(
-            url:url,
-            cachePolicy:kCachePolicy,
-            timeoutInterval:kTimeOutData)
-        urlMutableRequest.httpMethod = kMethod
-        urlMutableRequest.allowsCellularAccess = kCellularAccess
-        
-        let headersKeys:[String] = Array(headers.keys)
-        
-        for key:String in headersKeys
-        {
-            guard
-                
-                let header:String = headers[key]
-                
-            else
-            {
-                continue
-            }
-            
-            urlMutableRequest.setValue(
-                header,
-                forHTTPHeaderField:key)
-        }
-        
-        let urlRequest:URLRequest = urlMutableRequest as URLRequest
-        
-        let urlSession:URLSession = URLSession(configuration:configuration)
-        task = urlSession.dataTask(with:urlRequest)
+        task = session.dataTask(with:request)
         { (data:Data?, urlResponse:URLResponse?, error:Error?) in
             
-            if error != nil
+            let statusCode:Int = self.statusCode(urlResponse:urlResponse)
+            let results:MSearchResults?
+            
+            switch statusCode
             {
-                return
+            case self.kStatusCodeSuccess:
+                
+                results = self.parseData(data:data)
+                
+                break
+                
+            default:
+                
+                results = nil
+                
+                break
             }
             
             guard
                 
-                let dataStrong:Data = data
-                
+                let modelResults:MSearchResults = results
+            
             else
             {
                 return
             }
             
-            let json:Any
-            
-            do
-            {
-                try json = JSONSerialization.jsonObject(
-                    with:dataStrong,
-                    options:JSONSerialization.ReadingOptions.allowFragments)
-            }
-            catch
-            {
-                return
-            }
-            
-            let results:MSearchResults = MSearchResults(
-                json:json)
             self.controller?.resultsFound(
                 query:query,
-                modelResults:results)
+                modelResults:modelResults)
         }
         
         task?.resume()
-        urlSession.finishTasksAndInvalidate()
+        session.finishTasksAndInvalidate()
+    }
+    
+    private func parseData(data:Data?) -> MSearchResults?
+    {
+        guard
+            
+            let dataStrong:Data = data
+            
+        else
+        {
+            return nil
+        }
+        
+        let json:Any
+        
+        do
+        {
+            try json = JSONSerialization.jsonObject(
+                with:dataStrong,
+                options:JSONSerialization.ReadingOptions.allowFragments)
+        }
+        catch
+        {
+            return nil
+        }
+        
+        let results:MSearchResults = MSearchResults(
+            json:json)
+        
+        return results
     }
 }
