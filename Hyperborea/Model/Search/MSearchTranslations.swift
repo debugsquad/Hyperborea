995c8b1dbd043cf61ef23ce9e5bf9955c2f8797c
vauxhall <zero.griffin@gmail.com>
2017-04-05 14:09:19 -0500

    parsing translations

--- Hyperborea/Model/Search/MSearchTranslations.swift
+++ Hyperborea/Model/Search/MSearchTranslations.swift
@@ -3,7 +3,87 @@ import UIKit
 class MSearchTranslations
 {
     let attributedString:NSAttributedString
+    private static let kKeyExamples:String = "examples"
+    private static let kKeyTranslations:String = "translations"
+    private static let kKeyText:String = "text"
+    private static let kBreak:String = "\n"
+    private static let kExamplesFontSize:CGFloat = 17
+    private static let kExamplesTranslationFontSize:CGFloat = 17
     private let kKeyResults:String = "results"
+    private let kKeyWord:String = "word"
+    private let kKeyLexicalEntries:String = "lexicalEntries"
+    private let kKeyEntries:String = "entries"
+    private let kKeySenses:String = "senses"
+    private let kKeySubsenses:String = "subsenses"
+    private let kTranslationsFontSize:CGFloat = 20
+    private let kWordFontSize:CGFloat = 40
+    
+    private class func parseExamples(json:[String:Any]) -> NSAttributedString?
+    {
+        guard
+            
+            let jsonExamples:[Any] = json[kKeyExamples] as? [Any]
+            
+        else
+        {
+            return nil
+        }
+        
+        let attributesExample:[String:Any] = [
+            NSFontAttributeName:UIFont.italic(size:kExamplesFontSize),
+            NSForegroundColorAttributeName:UIColor(white:0.4, alpha:1)]
+        let attributesTranslation:[String:Any] = [
+            NSFontAttributeName:UIFont.regular(size:kExamplesTranslationFontSize),
+            NSForegroundColorAttributeName:UIColor(white:0.1, alpha:1)]
+        
+        let mutableString:NSMutableAttributedString = NSMutableAttributedString()
+        let stringBreak:NSAttributedString = NSAttributedString(
+            string:kBreak,
+            attributes:attributesExample)
+        
+        for jsonExample:Any in jsonExamples
+        {
+            guard
+                
+                let jsonExampleMap:[String:Any] = jsonExample as? [String:Any],
+                let jsonExampleText:String = jsonExampleMap[kKeyText] as? String,
+                let jsonExampleTranslations:[Any] = jsonExampleMap[kKeyTranslations] as? [Any]
+            
+            else
+            {
+                continue
+            }
+            
+            let stringText:NSAttributedString = NSAttributedString(
+                string:jsonExampleText,
+                attributes:attributesExample)
+            
+            mutableString.append(stringText)
+            mutableString.append(stringBreak)
+            
+            for jsonExampleTranslation:Any in jsonExampleTranslations
+            {
+                guard
+                
+                    let jsonTranslationMap:[String:Any] = jsonExampleTranslation as? [String:Any],
+                    let jsonTranslationText:String = jsonTranslationMap[kKeyText] as? String
+                
+                else
+                {
+                    continue
+                }
+                
+                let stringText:NSAttributedString = NSAttributedString(
+                    string:jsonTranslationText,
+                    attributes:attributesTranslation)
+                
+                mutableString.append(stringText)
+                mutableString.append(stringBreak)
+            }
+        }
+        
+        return mutableString
+    }
     
     init(json:Any)
     {
@@ -21,13 +101,149 @@ class MSearchTranslations
             return
         }
         
+        let attributesTranslations:[String:AnyObject] = [
+            NSFontAttributeName:UIFont.bold(size:kTranslationsFontSize),
+            NSForegroundColorAttributeName:UIColor.black]
+        let attributesWord:[String:Any] = [
+            NSFontAttributeName:UIFont.medium(size:kWordFontSize)]
+        var stringWord:NSAttributedString?
+        let stringBreak:NSAttributedString = NSAttributedString(
+            string:MSearchTranslations.kBreak,
+            attributes:attributesWord)
         
+        for jsonResult:Any in jsonResults
+        {
+            guard
+                
+                let jsonResultMap:[String:Any] = jsonResult as? [String:Any],
+                let jsonLexicalEntries:[Any] = jsonResultMap[kKeyLexicalEntries] as? [Any]
+                
+            else
+            {
+                continue
+            }
+            
+            if stringWord == nil
+            {
+                if let rawWord:String = jsonResultMap[kKeyWord] as? String
+                {
+                    stringWord = NSAttributedString(
+                        string:rawWord,
+                        attributes:attributesWord)
+                }
+            }
+            
+            for jsonLexicalEntry:Any in jsonLexicalEntries
+            {
+                guard
+                    
+                    let jsonLexicalEntryMap:[String:Any] = jsonLexicalEntry as? [String:Any],
+                    let jsonEntries:[Any] = jsonLexicalEntryMap[kKeyEntries] as? [Any]
+                    
+                else
+                {
+                    continue
+                }
+                
+                for jsonEntry:Any in jsonEntries
+                {
+                    guard
+                        
+                        let jsonEntryMap:[String:Any] = jsonEntry as? [String:Any],
+                        let jsonEntrySenses:[Any] = jsonEntryMap[kKeySenses] as? [Any]
+                        
+                    else
+                    {
+                        continue
+                    }
+                    
+                    for jsonEntrySense:Any in jsonEntrySenses
+                    {
+                        guard
+                            
+                            let jsonSenseMap:[String:Any] = jsonEntrySense as? [String:Any]
+                            
+                        else
+                        {
+                            continue
+                        }
+                        
+                        if let jsonTranslations:[Any] = jsonSenseMap[
+                            MSearchTranslations.kKeyTranslations] as? [Any]
+                        {
+                            let translationsString:NSMutableAttributedString = NSMutableAttributedString()
+                            
+                            for jsonTranslation:Any in jsonTranslations
+                            {
+                                guard
+                                
+                                    let jsonTranslationMap:[String:Any] = jsonTranslation as? [String:Any],
+                                    let jsonTranslationText:String = jsonTranslationMap[
+                                        MSearchTranslations.kKeyText] as? String
+                                
+                                else
+                                {
+                                    continue
+                                }
+                                
+                                let stringTranslation:NSAttributedString = NSAttributedString(
+                                    string:jsonTranslationText,
+                                    attributes:attributesTranslations)
+                                
+                                translationsString.append(stringTranslation)
+                                translationsString.append(stringBreak)
+                            }
+                            
+                            mutableString.insert(translationsString, at:0)
+                        }
+                        
+                        if let exampleString:NSAttributedString = MSearchTranslations.parseExamples(
+                            json:jsonSenseMap)
+                        {
+                            mutableString.append(exampleString)
+                        }
+                        
+                        if let jsonSubsenses:[Any] = jsonSenseMap[kKeySubsenses] as? [Any]
+                        {
+                            for jsonSubsense:Any in jsonSubsenses
+                            {
+                                guard
+                                
+                                    let jsonSubsenseMap:[String:Any] = jsonSubsense as? [String:Any],
+                                    let exampleString:NSAttributedString = MSearchTranslations.parseExamples(
+                                        json:jsonSubsenseMap)
+                                
+                                else
+                                {
+                                    continue
+                                }
+                                
+                                mutableString.append(exampleString)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        if let stringWord:NSAttributedString = stringWord
+        {
+            mutableString.insert(stringBreak, at:0)
+            mutableString.insert(stringWord, at:0)
+        }
         
         attributedString = mutableString
     }
     
     init()
     {
-        attributedString = NSAttributedString()
+        let string:String = NSLocalizedString("MSearchTranslations_notFound", comment:"")
+        let attributes:[String:AnyObject] = [
+            NSFontAttributeName:UIFont.regular(size:MSearchTranslations.kExamplesFontSize),
+            NSForegroundColorAttributeName:UIColor(white:0.4, alpha:1)]
+        
+        attributedString = NSAttributedString(
+            string:string,
+            attributes:attributes)
     }
 }
