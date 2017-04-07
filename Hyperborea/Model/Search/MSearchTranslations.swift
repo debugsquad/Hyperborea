1bab7eea828803edd7f31d65675d7fafc12e39ec
vauxhall <zero.griffin@gmail.com>
2017-04-07 18:01:19 -0500

    capitalized translation word

--- Hyperborea/Model/Search/MSearchTranslations.swift
+++ Hyperborea/Model/Search/MSearchTranslations.swift
@@ -135,8 +135,9 @@ class MSearchTranslations
             {
                 if let rawWord:String = jsonResultMap[kKeyWord] as? String
                 {
+                    let capitalizedWord:String = rawWord.capitalizedFirstLetter()
                     stringWord = NSAttributedString(
-                        string:rawWord,
+                        string:capitalizedWord,
                         attributes:attributesWord)
                 }
             }
