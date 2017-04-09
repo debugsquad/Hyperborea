da8841cba86f400cd2de41f048d7352e00944f53
vauxhall <zero.griffin@gmail.com>
2017-04-09 17:05:15 -0500

    controller view froob

--- Hyperborea/Controller/CFroobPlus.swift
+++ Hyperborea/Controller/CFroobPlus.swift
@@ -2,5 +2,12 @@ import UIKit
 
 class CFroobPlus:CController
 {
+    private weak var viewFroob:VFroobPlus!
     
+    override func loadView()
+    {
+        let viewFroob:VFroobPlus = VFroobPlus(controller:self)
+        self.viewFroob = viewFroob
+        view = viewFroob
+    }
 }
--- Hyperborea/View/Froob/VFroobPlus.swift
+++ Hyperborea/View/Froob/VFroobPlus.swift
@@ -2,5 +2,17 @@ import UIKit
 
 class VFroobPlus:VView
 {
+    private weak var controller:CFroobPlus!
     
+    override init(controller:CController)
+    {
+        super.init(controller:controller)
+        backgroundColor = UIColor.clear
+        self.controller = controller as? CFroobPlus
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
