db33b23908b1e2804cadfeddd05a2762821c20e1
vauxhall <zero.griffin@gmail.com>
2017-04-10 14:55:51 -0500

    settings items selectable

--- Hyperborea/Model/Settings/MSettingsItem.swift
+++ Hyperborea/Model/Settings/MSettingsItem.swift
@@ -4,10 +4,15 @@ class MSettingsItem
 {
     let reusableIdentifier:String
     let cellHeight:CGFloat
+    let selectable:Bool
     
-    init(reusableIdentifier:String, cellHeight:CGFloat)
+    init(
+        reusableIdentifier:String,
+        cellHeight:CGFloat,
+        selectable:Bool)
     {
         self.reusableIdentifier = reusableIdentifier
         self.cellHeight = cellHeight
+        self.selectable = selectable
     }
 }
